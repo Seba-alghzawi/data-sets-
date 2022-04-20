74af8e661eb26db258cb11daf858d9fb58e5ac81
Seba <seba.alghzawi1999@gmail.com>
2022-04-20 14:48:33 +0300

    20/4 wednesday

--- src/app/roles/roles.component.ts
+++ src/app/roles/roles.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder ,Validators,FormArray,FormControl} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 import { ServiceDataService } from '../service-data.service';
 @Component({
@@ -9,76 +9,69 @@ import { ServiceDataService } from '../service-data.service';
 })
 
 export class RolesComponent implements OnInit {
-  
-  selectedValue:any;
+  testNull=false;
   receivedRoles: any;
-  dynamicForm : FormGroup;
-  
-  childes:any;
-  childarray:Array<object>[]=[];
-  selected:boolean=false;
-  constructor(private rol:ServiceDataService,private formBuilder: FormBuilder) { 
-    
-    // interface child{
-    //   children: []
-    //   createDate: null
-    //   id: number
-    //   roleColor: number
-    //   roleDescAr: string
-    //   roleDescEn: string
-    //   roleID: number
-    //   surveyID: number
-    //   }
-
+  dynamicForm: FormGroup;
+  childes: any;
+  childarray: Array<{
+    children: []
+    createDate: null
+    id: number
+    roleColor: number
+    roleDescAr: string
+    roleDescEn: string
+    roleID: number
+    surveyID: number
+  }>[] = [];
+  selected: boolean = false;
+  constructor(private rol: ServiceDataService, private formBuilder: FormBuilder) {
     this.dynamicForm = this.formBuilder.group({})
   }
- 
+
   ngOnInit(): void {
-    this.rol.getroles().subscribe(data=>{
-      this.receivedRoles=data;
+    this.rol.getroles().subscribe(data => {
+      this.receivedRoles = data;
       console.log(this.receivedRoles)
-    
-      // if(this.selectedValue.roleID==null)
-      // {
-      //   console.log(this.selectedValue.children);
-      // }
+
     })
     this.dynamicForm = this.formBuilder.group({
       role: ['', Validators.required],
       roles: new FormArray([])
-  });
-}
+    });
+  }
 
 
-    get f() { return this.dynamicForm.controls; }
-    get t() { return this.f['roles' ]as FormArray; }
-    get roleFormGroups() { return this.t.controls as FormGroup[]; }
+  get f() { return this.dynamicForm.controls; }
+  get t() { return this.f['roles'] as FormArray; }
+  get roleFormGroups() { return this.t.controls as FormGroup[]; }
 
+
+
+  onChange1(e: any) {
+    this.childes = e.value.children;
+    this.childarray.push(this.childes);
+    console.log(this.childarray);
+    console.log(this.childes);
+    this.t.push(this.formBuilder.group({
+        subchild: (this.childes)
+      }));
     
+    console.log(this.dynamicForm.value)
 
 
-    onChange(e:any) {
-      
-       this.childes=e.value.children ;
-      //  let child2=this.childes.value.children;
-      this.childarray.push(this.childes);
-      console.log(this.childarray);
-      console.log(this.childes);
-        
-        this.t.push(this.formBuilder.group({
-          subchild: (this.childes)
-          }));
-      console.log(this.dynamicForm.value)
-
-      // if (this.t.length < numberOfTickets) {
-      //     for (let i = this.t.length; i < numberOfTickets; i++) {
-      //         this.t.push(this.formBuilder.group({
-      //             subRoles: ['', Validators.required],
-                 
-      //         }));
-      //     }
-      // } 
-    }
+  }
+  onChange2(e: any,index:number) {
+    
+    this.childes = e.value.children;
+    this.childarray.push(this.childes);
+    console.log(this.childarray);
+    console.log(this.childes);
+    this.t.push(this.formBuilder.group({
+        subchild: (this.childes)
+    }));
+    //  this.childarray.splice(index);
+    console.log(this.dynamicForm.value)
+  }
 
 
 
